92f5a28fc19a5b6d2d3894d7eb089bc485f41021
eating830928 <165301739+eating830928@users.noreply.github.com>
2024-03-30 22:36:52 +0800

    240330_ver1.0

---
+++ 240330.py
@@ -0,0 +1,30 @@
+#asdfasdfasdf
+
+# string = "Hello World Ivy"
+# a1 = 1
+# a2 = 2
+# a3 = 3
+# a4 = 4
+# a5 = 5
+# a6 = 6
+# a1000 = 1000
+
+
+
+#number = (1,2,3,4,5)
+#total = (sum(number))
+
+#print (total)
+
+#print("===========================")
+
+def sum_ivy(a, b):
+    result = a + b
+    return result
+
+a = sum_ivy(a, b)
+print(sum_ivy(3, 7))
+
+
+
+#print(sum_ivy(3,7)) #result
